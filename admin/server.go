58b07f88b5ebdfa3b7fc021baf1f752cc2bdcb39
Sean Harger <seanharger93@gmail.com>
2017-07-03 15:45:08 -0700

    Updated server for config package changes.
    The server now maintains state in a struct. It also parses the config
    file location from a flag and reports an error if none is set.
    
    Support for appending to the configuration file is coming soon.

--- admin/server.go
+++ admin/server.go
@@ -6,28 +6,50 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 
-	"github.com/wallaceicy06/sign-server/config"
+	"github.com/wallaceicy06/muni-sign/admin/config"
 )
 
 var templates = template.Must(template.ParseFiles("templates/index.html"))
 
+var configFilePath = flag.String("config_file", "", "the path to the file that stores the configuration for the sign")
 var port = flag.Int("port", 8080, "the port to serve this webserver")
+
+type Server struct {
+	cfg *config.SignConfig
+}
 
 func main() {
 	flag.Parse()
-	http.HandleFunc("/", rootHandler)
+
+	if *configFilePath == "" {
+		fmt.Fprintln(os.Stderr, "A config file path is required.")
+		os.Exit(1)
+	}
+
+	sc := config.NewSignConfig(*configFilePath)
+	server := &Server{
+		cfg: sc,
+	}
+	server.serve()
+}
+
+func (s *Server) serve() {
+	http.HandleFunc("/", s.rootHandler)
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil); err != nil {
 		log.Fatalf("Error starting web server on port %d: %v", port, err)
 	}
 }
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	config, err := config.GetConfig()
+func (s *Server) rootHandler(w http.ResponseWriter, r *http.Request) {
+	c, err := s.cfg.Get()
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Internal error: %v", err), http.StatusInternalServerError)
+		return
 	}
-	if err := templates.ExecuteTemplate(w, "index.html", config); err != nil {
+	if err := templates.ExecuteTemplate(w, "index.html", c); err != nil {
 		http.Error(w, fmt.Sprintf("Internal error: %v", err), http.StatusInternalServerError)
+		return
 	}
 }
