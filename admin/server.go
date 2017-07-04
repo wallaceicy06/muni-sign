249a6743378e0ce4a0be18219c230e4315c7da78
Sean Harger <seanharger93@gmail.com>
2017-07-04 12:36:05 -0700

    Refactor the server for better testability.
    Made the server struct unexported.
    Moved the creation of the server into a helper method.
    Added port as a property of the server struct.
    Refactored the serving process to start on a different thread and
    return the server variable for ease of shutting down the server. The
    main thread now listens on SIGINT signals and shuts down the server
    gracefully when it receives one.

--- admin/server.go
+++ admin/server.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 
 	"github.com/wallaceicy06/muni-sign/admin/config"
 )
@@ -16,8 +18,9 @@ var templates = template.Must(template.ParseFiles("templates/index.html"))
 var configFilePath = flag.String("config_file", "", "the path to the file that stores the configuration for the sign")
 var port = flag.Int("port", 8080, "the port to serve this webserver")
 
-type Server struct {
-	cfg config.SignConfig
+type server struct {
+	cfg  config.SignConfig
+	port int
 }
 
 func main() {
@@ -28,21 +31,39 @@ func main() {
 		os.Exit(1)
 	}
 
-	sc := config.NewFileSignConfig(*configFilePath)
-	server := &Server{
-		cfg: sc,
-	}
-	server.serve()
+	srv := newServer(*port, config.NewFileSignConfig(*configFilePath)).serve()
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt)
+	<-sigs
+	srv.Shutdown(context.Background())
+	os.Exit(0)
 }
 
-func (s *Server) serve() {
+func newServer(port int, cfg config.SignConfig) *server {
+	return &server{
+		port: port,
+		cfg:  cfg,
+	}
+}
+
+func (s *server) serve() *http.Server {
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%d", s.port),
+	}
+
 	http.HandleFunc("/", s.rootHandler)
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil); err != nil {
-		log.Fatalf("Error starting web server on port %d: %v", port, err)
-	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Printf("Error serving: %v", err)
+		}
+	}()
+
+	return srv
 }
 
-func (s *Server) rootHandler(w http.ResponseWriter, r *http.Request) {
+func (s *server) rootHandler(w http.ResponseWriter, r *http.Request) {
 	c, err := s.cfg.Get()
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Internal error: %v", err), http.StatusInternalServerError)
