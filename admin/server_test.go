81636b21619fda8180d81d36bf3c0acf5d0cfd83
Sean Harger <seanharger93@gmail.com>
2017-07-16 11:32:21 -0700

    Fix admin server tests to get the recorder result before inspecting.

--- admin/server_test.go
+++ admin/server_test.go
@@ -114,8 +114,10 @@ func TestGetConfig(t *testing.T) {
 			rec := &httptest.ResponseRecorder{}
 
 			srv.rootHandler(rec, test.req)
-			if rec.Code != test.wantCode {
-				t.Errorf("server response code unexpected, got %d want %d", rec.Code, test.wantCode)
+			res := rec.Result()
+
+			if res.StatusCode != test.wantCode {
+				t.Errorf("server response code unexpected, got %d want %d", res.StatusCode, test.wantCode)
 			}
 		})
 	}
@@ -176,8 +178,10 @@ func TestUpdateConfig(t *testing.T) {
 			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
 			srv.rootHandler(rec, req)
-			if rec.Code != test.wantCode {
-				t.Errorf("server response code unexpected, got %d want %d", rec.Code, test.wantCode)
+			res := rec.Result()
+
+			if res.StatusCode != test.wantCode {
+				t.Errorf("server response code unexpected, got %d want %d", res.StatusCode, test.wantCode)
 			}
 			if !proto.Equal(test.cfg.cfg, test.wantCfg) {
 				t.Errorf("configurations differ: got %v, want %v", test.cfg.cfg, test.wantCfg)
@@ -192,8 +196,10 @@ func TestInvalidMethod(t *testing.T) {
 
 	req := httptest.NewRequest(http.MethodDelete, "/", nil)
 	srv.rootHandler(rec, req)
-	if rec.Code != http.StatusMethodNotAllowed {
-		t.Errorf("rec.Code = %d, want %d", rec.Code, http.StatusMethodNotAllowed)
+	res := rec.Result()
+
+	if res.StatusCode != http.StatusMethodNotAllowed {
+		t.Errorf("StatusCode = %d, want %d", res.StatusCode, http.StatusMethodNotAllowed)
 	}
 }
 
